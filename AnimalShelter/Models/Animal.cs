45435dc7107f0aa1b2460632a7e8b7b43df680d8
Matt Stroud <lixingke06@gmail.com>
2020-05-27 16:20:18 -0700

    add boilerplate code

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string Date { get; set; }
+    public string Breed { get; set; }
+  }
+}
